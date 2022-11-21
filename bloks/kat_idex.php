58d3cfabde6914afae21844354dc6d867a0a6753
Dimitri1289 <sipios@yandex.ru>
2022-11-21 22:46:38 +1000

    каталог продукции на главной

---
+++ bloks/kat_idex.php
@@ -0,0 +1,161 @@
+<table>
+  <tr>
+    <td  class="lot"><h4><a href="../roza-vetrov.php">Тротуарная плитка 40х40х4 см.<br>
+      &quot;Роза-Ветров&quot;</a><br>
+    </h4>
+        <table class="lot_foto">
+          <tr>
+            <td style="width:48%"><a href="img/sidewalk/001.png" target="_blank"> <img style="width:154px; height:154px; padding-left:0px; padding-bottom:0px" src="../img/sidewalk/001.png" alt="Тротуарная плитка &quot;Роза-Ветров&quot;" /></a><br>
+            </td>
+            <td style="width:52%; text-align:left"><em>Параметры:</em><br>
+              &bull; 395х395х40мм<br>
+              &bull;  6.25 шт. в 1м&sup2;<br>
+              &bull; 75 кг. в 1м&sup2;<br>
+              &bull; 63 шт. в палете<br>
+              <em>Цена:</em><br>
+              &bull; 109.50 &#8381;. за шт.</td>
+          </tr>
+        </table>
+      <br>
+        <img style="width:40px; height:40px" src="../img/color/6.png" alt="Цвет: без красителя" title="Без красителя" /> &nbsp; <img  style="width:40px; height:40px" src="../img/color/1.png" alt="Цвет: коричневый" title="Коричневый" />&nbsp;&nbsp; <img style="width:40px; height:40px" src="../img/color/4.png" alt="Цвет: кирпичный" title="Кирпичный" />&nbsp;&nbsp; <img style="width:40px; height:40px" src="../img/color/5.png" alt="Цвет: золотой" title="Золотой" />&nbsp;&nbsp;&nbsp;<img style="width:40px; height:40px" src="../img/color/7.png" alt="Цвет: чёрный"  title="Чёрный" />&nbsp;&nbsp; <img style="width:40px; height:40px" src="../img/color/8.png" alt="Цвет: красный" title="Красный" /> <br>
+        <br>
+        <span class="price">Цена: 690.00 &#8381;/м&sup2;</span> <br>
+        <br>
+        <a href="?forma_5#openModal"><img style="width:224px; height:56px" src="../img/all/zakaz.png" alt="Купить тротуарную плитку &quot;Роза-ветров&quot;" /></a> <br>
+        <br></td>
+    <td style="width:40px">&nbsp;</td>
+    <td class="lot"><h4><a href="../l_trotuar.php">Тротуарная плитка 40х40х4 см.<br>
+      &quot;Львовский тротуар&quot;</a><br>
+    </h4>
+        <table class="lot_foto">
+          <tr>
+            <td style="width:48%"><a href="img/sidewalk/002.png" target="_blank"> <img style="width:154px; height:154px; text-align:left" src="../img/sidewalk/002.png" alt="Тротуарная плитка &quot;Львовский тротуар&quot;" /></a><br>
+            </td>
+            <td style="width:52%; text-align:left"><em>Параметры:</em><br>
+              &bull; 385х385х40мм<br>
+              &bull;  6.6 шт. в 1м&sup2;<br>
+              &bull; 78 кг. в 1м&sup2;<br>
+              &bull; 66 шт. в палете<br>
+              <em>Цена:</em><br>
+              &bull; 103.00 &#8381;. за шт.</td>
+          </tr>
+        </table>
+      <br>
+        <img style="width:40px; height:40px" src="../img/color/6.png" alt="Цвет: без красителя" title="Без красителя" /> &nbsp; <img  style="width:40px; height:40px" src="../img/color/1.png" alt="Цвет: коричневый" title="Коричневый" />&nbsp;&nbsp; <img style="width:40px; height:40px" src="../img/color/4.png" alt="Цвет: кирпичный" title="Кирпичный" />&nbsp;&nbsp; <img style="width:40px; height:40px" src="../img/color/5.png" alt="Цвет: золотой" title="Золотой" />&nbsp;&nbsp;&nbsp;<img style="width:40px; height:40px" src="../img/color/7.png" alt="Цвет: чёрный"  title="Чёрный" />&nbsp;&nbsp; <img style="width:40px; height:40px" src="../img/color/8.png" alt="Цвет: красный" title="Красный" /> <br>
+        <br>
+        <span class="price">Цена: 680.00 &#8381;/м&sup2;</span> <br>
+        <br>
+        <a href="?forma_6#openModal"><img style="width:224px; height:56px" src="../img/all/zakaz.png" alt="Купить тротуарную плитку &quot;Львовский тротуар&quot;" /><br>
+        <br>
+      </a></td>
+  </tr>
+  <tr>
+    <td><br>
+        <br></td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td  class="lot"><h4><a href="../kaliforniya.php">Тротуарная плитка 30х30х3.5 см.<br>
+      &quot;Калифорния&quot;</a></h4>
+        <table class="lot_foto">
+          <tr>
+            <td style="width:48%"><a href="img/sidewalk/003.jpg" target="_blank"><img style="width:154px; height:154px; padding-left:0px; padding-bottom:0px" src="../img/sidewalk/003.jpg" alt="Тротуарная плитка &quot;Калифорния&quot;" /></a><br>
+            </td>
+            <td style="width:52%; text-align:left"><em>Параметры:</em><br>
+              &bull; 285х285х35мм<br>
+              &bull;  12 шт. в 1м&sup2;<br>
+              &bull; 78 кг. в 1м&sup2;<br>
+              &bull; 120 шт. в палете<br>
+              <em>Цена:</em><br>
+              &bull; 56.00 &#8381;. за шт.</td>
+          </tr>
+        </table>
+      <br>
+        <img style="width:40px; height:40px" src="../img/color/6.png" alt="Цвет: без красителя" title="Без красителя" /> &nbsp; <img  style="width:40px; height:40px" src="../img/color/1.png" alt="Цвет: коричневый" title="Коричневый" />&nbsp;&nbsp; <img style="width:40px; height:40px" src="../img/color/4.png" alt="Цвет: кирпичный" title="Кирпичный" />&nbsp;&nbsp; <img style="width:40px; height:40px" src="../img/color/5.png" alt="Цвет: золотой" title="Золотой" />&nbsp;&nbsp;&nbsp;<img style="width:40px; height:40px" src="../img/color/7.png" alt="Цвет: чёрный"  title="Чёрный" />&nbsp;&nbsp; <img style="width:40px; height:40px" src="../img/color/8.png" alt="Цвет: красный" title="Красный" /> <br>
+        <br>
+        <span class="price">Цена: 670.00 &#8381;/м&sup2;</span> <br>
+        <br>
+        <a href="?forma_1#openModal"><img style="width:224px; height:56px" src="../img/all/zakaz.png" alt="Купить тротуарную плитку &quot;Калифорния&quot;" /></a> <br>
+        <br></td>
+    <td style="width:40px">&nbsp;</td>
+    <td class="lot"><h4><a href="../giraf.php">Тротуарная плитка 30х30х3 см.<br>
+      &quot;Жираф&quot;</a></h4>
+        <table class="lot_foto">
+          <tr>
+            <td style="width:48%"><a href="img/sidewalk/05.png" target="_blank"> <img style="width:154px; height:154px; text-align:left" src="../img/sidewalk/05.png" alt="Тротуарная плитка &quot;Жираф&quot;" /></a><br>
+            </td>
+            <td style="width:52%; text-align:left"><em>Параметры:</em><br>
+              &bull; 295х295х30мм<br>
+              &bull;  11.11 шт. в 1м&sup2;<br>
+              &bull; 64 кг. в 1м&sup2;<br>
+              &bull; 111 шт. в палете<br>
+              <em>Цена:</em><br>
+              &bull; 58.50 &#8381;. за шт.</td>
+          </tr>
+        </table>
+      <br>
+        <img style="width:40px; height:40px" src="../img/color/6.png" alt="Цвет: без красителя" title="Без красителя" /> &nbsp; <img  style="width:40px; height:40px" src="../img/color/1.png" alt="Цвет: коричневый" title="Коричневый" />&nbsp;&nbsp; <img style="width:40px; height:40px" src="../img/color/4.png" alt="Цвет: кирпичный" title="Кирпичный" />&nbsp;&nbsp; <img style="width:40px; height:40px" src="../img/color/5.png" alt="Цвет: золотой" title="Золотой" />&nbsp;&nbsp;&nbsp;<img style="width:40px; height:40px" src="../img/color/7.png" alt="Цвет: чёрный"  title="Чёрный" />&nbsp;&nbsp; <img style="width:40px; height:40px" src="../img/color/8.png" alt="Цвет: красный" title="Красный" /> <br>
+        <br>
+        <span class="price">Цена: 650.00 &#8381;/м&sup2;</span> <br>
+        <br>
+        <a href="?forma_2#openModal"><img style="width:224px; height:56px" src="../img/all/zakaz.png" alt="Купить тротуарную плитку &quot;Жирафв&quot;" /><br>
+        <br>
+      </a></td>
+  </tr>
+  <tr>
+    <td><br>
+        <br></td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td  class="lot"><h4><a href="../galka.php">Тротуарная плитка 30х30х3 см.<br>
+      &quot;Галька&quot;</a></h4>
+        <table class="lot_foto">
+          <tr>
+            <td style="width:48%"><a href="img/sidewalk/016.png" target="_blank"> <img style="width:154px; height:154px; padding-left:0px; padding-bottom:0px" src="../img/sidewalk/016.png" alt="Тротуарная плитка &quot;Галька&quot;" /></a><br>
+            </td>
+            <td style="width:52%; text-align:left"><em>Параметры:</em><br>
+              &bull; 295х295х30мм<br>
+              &bull;  11.11 шт. в 1м&sup2;<br>
+              &bull; 64 кг. в 1м&sup2;<br>
+              &bull; 111 шт. в палете<br>
+              <em>Цена:</em><br>
+              &bull; 62.10 &#8381;. за шт.</td>
+          </tr>
+        </table>
+      <br>
+        <img style="width:40px; height:40px" src="../img/color/6.png" alt="Цвет: без красителя" title="Без красителя" /> &nbsp; <img  style="width:40px; height:40px" src="../img/color/1.png" alt="Цвет: коричневый" title="Коричневый" />&nbsp;&nbsp; <img style="width:40px; height:40px" src="../img/color/4.png" alt="Цвет: кирпичный" title="Кирпичный" />&nbsp;&nbsp; <img style="width:40px; height:40px" src="../img/color/5.png" alt="Цвет: золотой" title="Золотой" />&nbsp;&nbsp;&nbsp;<img style="width:40px; height:40px" src="../img/color/7.png" alt="Цвет: чёрный"  title="Чёрный" />&nbsp;&nbsp; <img style="width:40px; height:40px" src="../img/color/8.png" alt="Цвет: красный" title="Красный" /> <br>
+        <br>
+        <span class="price">Цена: 690.00 &#8381;/м&sup2;</span> <br>
+        <br>
+        <a href="?forma_3#openModal"><img style="width:224px; height:56px" src="../img/all/zakaz.png" alt="Купить тротуарную плитку &quot;Галька&quot;" /></a> <br>
+        <br></td>
+    <td style="width:40px">&nbsp;</td>
+    <td class="lot"><h4><a href="../gotika.php">Тротуарная плитка 30х30х5 см.<br>
+      &quot;Готика&quot;</a></h4>
+        <table class="lot_foto">
+          <tr>
+            <td style="width:48%"><a href="img/sidewalk/02.png" target="_blank"> <img style="width:154px; height:154px; text-align:left" src="../img/sidewalk/02.png" alt="Тротуарная плитка &quot;Готика&quot;" /></a><br>
+            </td>
+            <td style="width:52%; text-align:left"><em>Параметры:</em><br>
+              &bull; 295х295х50мм<br>
+              &bull;  11.11 шт. в 1м&sup2;<br>
+              &bull; 98 кг. в 1м&sup2;<br>
+              &bull; 111 шт. в палете<br>
+              <em>Цена:</em><br>
+              &bull; 70.20 &#8381;. за шт.</td>
+          </tr>
+        </table>
+      <br>
+        <img style="width:40px; height:40px" src="../img/color/6.png" alt="Цвет: без красителя" title="Без красителя" /> &nbsp; <img  style="width:40px; height:40px" src="../img/color/1.png" alt="Цвет: коричневый" title="Коричневый" />&nbsp;&nbsp; <img style="width:40px; height:40px" src="../img/color/4.png" alt="Цвет: кирпичный" title="Кирпичный" />&nbsp;&nbsp; <img style="width:40px; height:40px" src="../img/color/5.png" alt="Цвет: золотой" title="Золотой" />&nbsp;&nbsp;&nbsp;<img style="width:40px; height:40px" src="../img/color/7.png" alt="Цвет: чёрный"  title="Чёрный" />&nbsp;&nbsp; <img style="width:40px; height:40px" src="../img/color/8.png" alt="Цвет: красный" title="Красный" /> <br>
+        <br>
+        <span class="price">Цена: 780.00 &#8381;/м&sup2;</span> <br>
+        <br>
+        <a href="?forma_4#openModal"><img style="width:224px; height:56px" src="../img/all/zakaz.png" alt="Купить тротуарную плитку &quot;Готика&quot;" /><br>
+        <br>
+        </a>
+        <div id="bruschatka"></div></td>
+  </tr>
+</table>
